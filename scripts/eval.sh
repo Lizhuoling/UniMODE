81f0405ebd78c3b6a99a059825db582c1959ead3
Zhuoling Li <1105069197@qq.com>
2023-06-30 14:10:19 +0800

    2023.6.30-sub6

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -4,5 +4,5 @@ CUDA_VISIBLE_DEVICES=3 python tools/train_net.py \
   --eval-only \
   --config-file configs/$exp_id.yaml \
   OUTPUT_DIR output/$exp_id \
-  #MODEL.WEIGHTS output/$exp_id/model_recent.pth
+  MODEL.WEIGHTS output/$exp_id/model_recent.pth
   #MODEL.WEIGHTS cubercnn://omni3d/cubercnn_DLA34_FPN.pth
