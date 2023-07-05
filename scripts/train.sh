971a65b314613ba7f1a0c91ca49c0b64139825be
Zhuoling Li <1105069197@qq.com>
2023-07-05 16:25:47 +0800

    PETR-xyz

--- scripts/train.sh
+++ scripts/train.sh
@@ -3,7 +3,7 @@ exp_id=OV_base2_omni3d_out_vov_optim_noGLIP_detr
 CUDA_VISIBLE_DEVICES=2,3 python tools/train_net.py \
   --config-file configs/$exp_id.yaml \
   --num-gpus 2 \
-  --dist-url tcp://127.0.0.1:12345 \
+  --dist-url tcp://127.0.0.1:12346 \
   OUTPUT_DIR output/$exp_id \
   #--resume \
   #MODEL.WEIGHTS_PRETRAIN output/OV_base2_omni3d_out_vov_visualfuse_loadpretrained/model_recent.pth
