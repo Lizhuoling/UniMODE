83ba097fed662ff67c3707561ad19735bd78b287
lizhuoling <1105069197@qq.com>
2023-08-18 21:18:27 +0800

    2023.8.18-sub5

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,13 +1,13 @@
-exp_id=TwoStage_base2_nusar_voxelfeat_h2
+exp_id=TwoStage_base2_nusar_voxelfeat_h1
 
-#ulimit -n 1024000
+#ulimit -n 65536
 
 python tools/train_net.py \
   --config-file configs/$exp_id.yaml \
   --num-gpus 2 \
   --num-machines 2 \
   --machine-rank 0 \
-  --dist-url tcp://ji-jupyter-7115764936211730432-worker-1.ji-jupyter-7115764936211730432:12345 \
+  --dist-url tcp://ji-jupyter-7115764936211730432-master-0.ji-jupyter-7115764936211730432:12345 \
   OUTPUT_DIR output/$exp_id \
   #--resume \
   #MODEL.WEIGHTS_PRETRAIN output/$exp_id/model_recent.pth \
