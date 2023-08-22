2c4a3bf555a5f5ceb2222e7568fdf0b80f598030
Zhuoling Li <1105069197@qq.com>
2023-08-22 13:10:14 +0800

    2023.8.22-sub3

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,11 +1,11 @@
-exp_id=TwoStage_base3_nusar
+exp_id=TwoStage_base3_omni3d
 
 #ulimit -n 65536
 
 python tools/train_net.py \
   --config-file configs/$exp_id.yaml \
   --num-gpus 4 \
-  --num-machines 1 \
+  --num-machines 2 \
   --machine-rank 0 \
   --dist-url tcp://127.0.0.1:12345 \
   OUTPUT_DIR output/$exp_id \
