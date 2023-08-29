ca52de297a8870e44e3679a654e6f8727ff6d4c4
Zhuoling Li <1105069197@qq.com>
2023-08-29 09:15:33 +0800

    2023.8.29-sub2

--- scripts/train.sh
+++ scripts/train.sh
@@ -5,9 +5,9 @@ exp_id=TwoStage_base4_omni3d
 python tools/train_net.py \
   --config-file configs/$exp_id.yaml \
   --num-gpus 4 \
-  --num-machines 1 \
+  --num-machines 2 \
   --machine-rank 0 \
-  --dist-url tcp://ji-jupyter-6937317828539244544-master-0.ji-jupyter-6937317828539244544:12345 \
+  --dist-url tcp://ji-jupyter-6968815243230736384-worker-1.ji-jupyter-6968815243230736384:12345 \
   OUTPUT_DIR output/$exp_id \
   #--resume \
   #MODEL.WEIGHTS_PRETRAIN output/$exp_id/model_recent.pth \
