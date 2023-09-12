9df7e536cf530ea1c2ea3723e9fcc5684394629b
Zhuoling Li <1105069197@qq.com>
2023-09-12 14:38:01 +0800

    2023.9.12-sub1

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,13 +1,13 @@
-exp_id=TwoStage_base6_omni3d
+exp_id=TwoStage_base6_omni3dtest_depthloss
 
 #ulimit -n 65536
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/train_net.py \
   --config-file configs/$exp_id.yaml \
   --num-gpus 4 \
-  --num-machines 2 \
+  --num-machines 1 \
   --machine-rank 0 \
-  --dist-url tcp://ji-jupyter-6937317828539244544-master-0.ji-jupyter-6937317828539244544:12345 \
+  --dist-url tcp://127.0.0.1:12345 \
   OUTPUT_DIR output/$exp_id \
   #--resume \
   #MODEL.WEIGHTS_PRETRAIN output/$exp_id/model_recent.pth \
