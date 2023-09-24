1db06cd9dc2784a8e1eef7e72b6a382386d0a275
Zhuoling Li <1105069197@qq.com>
2023-09-24 09:21:55 +0800

    2023.9.24-sub2

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,4 +1,4 @@
-exp_id=TwoStage_Baseline_arobj
+exp_id=TwoStage_baseline_omni3dtest_dla34
 
 #ulimit -n 65536
 
@@ -7,10 +7,8 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/train_net.py \
   --num-gpus 4 \
   --num-machines 1 \
   --machine-rank 0 \
-  --resume \
   --dist-url tcp://127.0.0.1:12345 \
   OUTPUT_DIR output/$exp_id \
-  MODEL.WEIGHTS_PRETRAIN output/$exp_id/model_recent.pth \
   #--resume \
   #MODEL.WEIGHTS_PRETRAIN output/$exp_id/model_recent.pth \
 
