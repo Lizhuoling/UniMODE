1d89aa34043499368d2396541e0367e720b36586
lizhuoling <1105069197@qq.com>
2023-10-06 20:47:50 +0800

    2023.10.6-sub1

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,16 +1,16 @@
-exp_id=mm_tinyomni3d_convnext_global
+exp_id=mm_omni3d_convnext_global
 
 #ulimit -n 65536
 
-CUDA_VISIBLE_DEVICES=2,3 python tools/train_net.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3 python tools/train_net.py \
   --config-file configs/$exp_id.yaml \
-  --num-gpus 2 \
-  --num-machines 1 \
+  --num-gpus 4 \
+  --num-machines 2 \
   --machine-rank 0 \
-  --resume \
-  --dist-url tcp://127.0.0.1:12345 \
+  --dist-url tcp://10.106.11.4:30891 \
   OUTPUT_DIR output/$exp_id \
-  MODEL.WEIGHTS_PRETRAIN output/$exp_id/model_recent.pth \
+  #--resume \
+  #MODEL.WEIGHTS_PRETRAIN output/$exp_id/model_recent.pth \
 
   #--dist-url tcp://ji-jupyter-6937317828539244544-master-0.ji-jupyter-6937317828539244544:12345 \
   #--dist-url tcp://127.0.0.1:12345 \
