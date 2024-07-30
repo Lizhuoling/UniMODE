75d6b0d66cc3f11551cb3d94bdd509627e31813e
Li Zhuoling <1105069197@qq.com>
2024-07-30 12:48:20 +0800

    update

--- scripts/eval.sh
+++
@@ -1,8 +0,0 @@
-exp_id=MM_UniMODE
-
-CUDA_VISIBLE_DEVICES=1 python tools/train_net.py \
-  --eval-only \
-  --config-file configs/$exp_id.yaml \
-  OUTPUT_DIR output/$exp_id \
-  MODEL.WEIGHTS output/$exp_id/model_recent.pth
-  #MODEL.WEIGHTS cubercnn://omni3d/cubercnn_DLA34_FPN.pth
